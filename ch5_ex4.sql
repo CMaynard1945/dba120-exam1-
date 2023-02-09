97643e8a48e573cedd5f6fd32c1439991e9c4e9c
Colton <122292816+CMaynard1945@users.noreply.github.com>
2023-02-09 14:58:02 -0500

    Create ch5_ex4.sql

---
+++ ch5_ex4.sql
@@ -0,0 +1,4 @@
+INSERT INTO invoices
+	(invoice_id, vendor_id, invoice_number, invoice_date, invoice_total, payment_total, credit_total, terms_id, invoice_due_date, payment_date)
+VALUES
+	(DEFAULT, 32, 'AX-014-027', '2018-08-01', 434.58, 0.00, 0.00, 2, '2018-08-31', NULL)
