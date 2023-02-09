bae382501bf7bad45acf232fda4422c3315cc72a
Colton <122292816+CMaynard1945@users.noreply.github.com>
2023-02-09 15:16:53 -0500

    Create ch5_ex5.sql

---
+++ ch5_ex5.sql
@@ -0,0 +1,5 @@
+INSERT INTO invoice_line_items
+	(invoice_id, invoice_sequence, account_number, line_item_amount, line_item_description)
+VALUES
+	(115, 1, 160, 180.23, 'Hard Drive'),
+  (115, 2, 527, 254.35, 'Exchange Server update')
